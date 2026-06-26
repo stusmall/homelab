ef4e9a873511aa96540e26edba26f2d8172b0e68
Stu Small <stuart.alan.small@gmail.com>
2026-06-26 17:54:38 -0600

    I think this should fix the elastic deployment

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -14,4 +14,4 @@ wait_for() {
 argocd login --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure localhost:8443
 argocd app create apps --dest-server https://kubernetes.default.svc --repo https://github.com/stusmall/homelab.git --path helm
 argocd app sync apps
-wait_for "kibana" kubectl rollout status deployment --namespace elastic kibana-kb-http
+wait_for "kibana" kubectl rollout status deployment --namespace elastic kibana-kb
