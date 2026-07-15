2b45125b14c11529162a7fa8ba1e925fa0b9e20b
Stu Small <stuart.alan.small@gmail.com>
2026-07-15 07:48:59 -0600

    Re-enable network policies and tweak terraform

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -18,32 +18,7 @@ provider "elasticstack" {
   }
 }
 
-resource "elasticstack_elasticsearch_index" "my_index" {
-  name = "my-index"
-
-  alias = [{
-    name = "my_alias_1"
-    }, {
-    name = "my_alias_2"
-    filter = jsonencode({
-      term = { "user.id" = "developer" }
-    })
-  }]
-
-  mappings = jsonencode({
-    properties = {
-      field1 = { type = "keyword" }
-      field2 = { type = "text" }
-      field3 = {
-        properties = {
-          inner_field1 = { type = "text", index = false }
-          inner_field2 = { type = "integer", index = false }
-        }
-      }
-    }
-  })
-
-  number_of_shards   = 1
-  number_of_replicas = 1
-  search_idle_after  = "20s"
+resource "elasticstack_elasticsearch_data_stream_lifecycle" "metrics_data_streams" {
+  name           = "metrics-*"
+  data_retention = "7d"
 }
