1a807ffa3980c3f135baecb726969558c9dcc993
Stu Small <stuart.alan.small@gmail.com>
2026-07-18 14:47:57 -0600

    Changing repo set up

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -11,7 +11,11 @@ wait_for() {
   echo "$desc ready."
 }
 
+#Load in secrets
+source .env
+
 argocd login --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure localhost:8443
+argocd repo add dhi.io   --type helm   --name dhi   --enable-oci   --username $DOCKER_USERNAME   --password $DOCKER_PAT
 argocd app create apps --dest-server https://kubernetes.default.svc --repo https://github.com/stusmall/homelab.git --path helm
 argocd app sync apps
 # might need to add a second sync since things seems to be getting caught up on CRDs
