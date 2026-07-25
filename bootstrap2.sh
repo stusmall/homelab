7bd9de27ccc56cf31e8fcf66273579b999ef3ec8
Stu Small <stuart.alan.small@gmail.com>
2026-07-25 15:33:21 -0600

    fix cert-manager version

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -15,7 +15,6 @@ wait_for() {
 source .env
 
 argocd login --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure localhost:8443
-argocd repo add dhi.io   --type helm   --name dhi   --enable-oci   --username $DOCKER_USERNAME   --password $DOCKER_PAT
 argocd app create apps --dest-server https://kubernetes.default.svc --repo https://github.com/stusmall/homelab.git --path helm
 argocd app sync apps
 # might need to add a second sync since things seems to be getting caught up on CRDs
