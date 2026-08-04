8c49d04dcab0e2f2c9fd697851b604299b9aa240
Stu Small <stuart.alan.small@gmail.com>
2026-08-03 21:11:00 -0600

    Increase resources

--- secrets.sh
+++ secrets.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 ARGO_PW=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 KEYCLOAK_PW=$( kubectl -n keycloak get secret keycloak -o jsonpath="{.data.admin-password}" | base64 -d)
+ELASTIC_PW=$(kubectl get secret -n elastic elastic-cluster-es-elastic-user -o jsonpath='{.data.elastic}' | base64 -d)
 echo "ArgoCD admin: $ARGO_PW"
 echo "Keycloak admin: $KEYCLOAK_PW"
+echo "Elastic admin: $ELASTIC_PW"
