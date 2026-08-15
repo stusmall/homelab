1f628053f912f0f6df9a495b50996950e9431c6e
Stu Small <stuart.alan.small@gmail.com>
2026-08-15 15:04:32 -0600

    Try disabling cnp

--- browser.sh
+++ browser.sh
@@ -1,3 +1,2 @@
 chromium \
-    --host-resolver-rules="MAP argocd.thenoodledragonlair.com $(kubectl get ingress -n argocd agrocd-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}")" \
-    --host-resolver-rules="MAP keycloak.thenoodledragonlair.com $(kubectl get ingress -n keycloak keycloak-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+    --host-resolver-rules="MAP argocd.thenoodledragonlair.com $(kubectl get ingress -n argocd agrocd-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}"), MAP keycloak.thenoodledragonlair.com $(kubectl get ingress -n keycloak keycloak-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
