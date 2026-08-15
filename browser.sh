8a80b08eb78c2b1fc064372929f73ad8d5d08d2b
Stu Small <stuart.alan.small@gmail.com>
2026-08-15 14:29:40 -0600

    Update network policy

--- browser.sh
+++ browser.sh
@@ -1,1 +1,3 @@
-chromium --host-resolver-rules="MAP argocd.thenoodledragonlair.com $(kubectl get ingress -n argocd agrocd-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+chromium \
+    --host-resolver-rules="MAP argocd.thenoodledragonlair.com $(kubectl get ingress -n argocd agrocd-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}")" \
+    --host-resolver-rules="MAP keycloak.thenoodledragonlair.com $(kubectl get ingress -n keycloak keycloak-ingress  -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
